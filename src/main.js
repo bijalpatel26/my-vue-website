325fa473038dcae3ac07d70699f8b43f6c48bbaf
bhpatel26 <bijal.patel@locushealth.com>
2021-03-27 19:48:37 -0400

    added new files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as firebase from 'firebase/app'
+import 'firebase/analytics'
+import VueMeta from 'vue-meta'
 
 Vue.config.productionTip = false
+
+Vue.use(VueMeta)
+var config = {
+  apiKey: 'AIzaSyCTOyYY-0sewMZHr4EMiDntui2_thczkUg',
+  projectId: 'my-website-8f6a4',
+  appId: '1:588740314554:web:5d2e08493ed85bbd370983',
+  measurementId: 'G-P08KP2P2K2'
+}
+
+firebase.initializeApp(config)
+Vue.prototype.$analytics = firebase.analytics();
 
 new Vue({
   render: h => h(App),
